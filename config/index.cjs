f583942c06a80e5632d20b7315414bfdd33dcd0a
Chopper121214 <135355401+Chopper121214@users.noreply.github.com>
2023-06-03 00:43:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ USE_PASSAGE: 'push-deer',
       // 想要发送的人的名字
       name: '小Q',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU23122T9WmlbPNVAn9mWCT385yXwX1Uy7qj3YOR',
+      id: 'PDU23128TqRqBAgr8wZz0VKwwBhoHU33JqfC3khpr',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
